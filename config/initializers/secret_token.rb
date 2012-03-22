237f647f416bab2066943796a863f3ff3c0230cb
Cyril Fluck <cfluck@pivotal-guest-459.sf.pivotallabs.com>
2012-03-22 11:53:12 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shapes::Application.config.secret_token = '40a85380b86c01d8ef171f53f79ad06cd875ec4ad122d5619d7f71d261a4f89402f88fbfaecae50b74cff4e9117f1953c9c517b7433b843bcf5fef3cad710adc'
